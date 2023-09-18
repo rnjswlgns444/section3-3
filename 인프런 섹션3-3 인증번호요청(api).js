343c0ff3893e0a5ba65e245d0b5e54ca5464f2d2
rnjswlgns444 <121981638+rnjswlgns444@users.noreply.github.com>
2023-09-18 22:35:04 +0900

    Create 인프런 섹션3-3 인증번호요청(api).js

---
+++ 인프런 섹션3-3 인증번호요청(api).js
@@ -0,0 +1,13 @@
+function createTokenOfPhone(myphone){
+  //1.휴대폰 번호 자릿수 맞는지 확인하기.
+  if(myphone.length !==10 && myphone.length !== 11){
+    console.log("에러 발생!! 핸드폰 번호를 제대로 입력해 주세요!")
+    return
+  }
+
+  //2. 핸드폰 토큰 6자리 만들기.
+
+  //3. 핸드폰번호에 토큰 전송하기.
+}
+
+creatTokenOfPhone("01012345678")
