4baa4a963a67a3c6670400508e0e1d6066f76f51
rnjswlgns444 <121981638+rnjswlgns444@users.noreply.github.com>
2023-09-18 22:55:54 +0900

    Create phone.js

---
+++ phone.js
@@ -0,0 +1,27 @@
+function checkValidationPhone(myphone) {  //번호 검증
+    if (myphone.length !== 10 && myphone.length !== 11) {
+        console.log('에러 발생!!! 핸드폰 번호를 제대로 입력해 주세요!!!');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function getToken(count) {  //6자리 토큰 만드는 함수
+    if (count === undefined) {
+        console.log('에러 발생!!! 갯수를 제대로 입력해 주세요!!!');
+        return;
+    } else if (count <= 0) {
+        console.log('에러 발생!!! 갯수가 너무 적습니다!!!');
+        return;
+    } else if (count > 10) {
+        console.log('에러 발생!!! 갯수가 너무 많습니다!!!');
+        return;
+    }
+    const result = String(Math.floor(Math.random() * 10 ** count)).padStart(
+        count,
+        '0',
+    );
+    return result;
+    // console.log(result)
+}
